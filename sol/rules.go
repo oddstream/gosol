e9e760ea97bb93db1f5e87817a19f7fcae43123a
Oddstream <oddstream@googlemail.com>
2021-03-16 09:27:33 +0000

    rules

--- sol/rules.go
+++ sol/rules.go
@@ -7,6 +7,48 @@ import (
 	"oddstream.games/gosol/util"
 )
 
+func englishRules(rules, flags int) string {
+	suit := rules / 10
+	rank := rules % 10
+
+	var s string
+	switch suit {
+	case 0:
+		s = "not allowed."
+	case 1:
+		s = "regardless of suit"
+	case 2:
+		s = "in suit"
+	case 3:
+		s = "in color"
+	case 4:
+		s = "in alternate colors"
+	case 5:
+		s = "in any other suit"
+	}
+	switch rank {
+	case 0:
+	case 1:
+		s = s + " and up, eg a 10 goes on a 9."
+		if flags&1 == 1 {
+			s = s + " Aces are allowed on Kings."
+		}
+	case 2:
+		s = s + " and down, eg a 9 goes on a 10."
+		if flags&1 == 1 {
+			s = s + " Kings are allowed on Aces."
+		}
+	case 4:
+		s = s + " and either up or down."
+		if flags&1 == 1 {
+			s = s + " Aces and Kings are allowed on top of each other."
+		}
+	case 5:
+		s = s + " regardless of rank."
+	}
+	return s
+}
+
 func (b *Baize) rulesContents() []string {
 
 	uniquePiles := []string{}
@@ -20,7 +62,6 @@ func (b *Baize) rulesContents() []string {
 	}
 
 	rules := []string{variantDescription(b.Variant)}
-	rules = append(rules, "This is a very long line of text for testing the word wrap widget to see how it handles longer lines that wrap and everything")
 
 	for _, pileClass := range uniquePiles {
 		p := b.findPile(pileClass)
@@ -64,6 +105,48 @@ func (b *Baize) rulesContents() []string {
 				targetClass = "Tableau"
 			}
 			fmt.Fprintf(&str, "Clicking on the stock will transfer one card to each %s. ", targetClass)
+		case "Waste":
+			fmt.Fprint(&str, "Waste: Cards can be be moved from here to Cells, Tableaux or Foundations.")
+		case "Foundation":
+			fmt.Fprint(&str, "Foundation: Build cards ")
+			fmt.Fprint(&str, englishRules(p.buildRules, p.buildFlags))
+		case "FoundationSpider":
+			fmt.Fprint(&str, "Foundation: Build cards ")
+			fmt.Fprint(&str, englishRules(p.buildRules, p.buildFlags))
+			fmt.Fprint(&str, ". Only a set of 13 cards are allowed to be moved here.")
+		case "Tableau":
+			if p.buildRules == p.dragRules {
+				fmt.Fprint(&str, "Tableau: Build cards ")
+				fmt.Fprint(&str, englishRules(p.buildRules, p.buildFlags))
+			} else {
+				fmt.Fprint(&str, "Tableau: Build cards ")
+				fmt.Fprint(&str, englishRules(p.buildRules, p.buildFlags))
+				fmt.Fprint(&str, ". Move cards ")
+				fmt.Fprint(&str, englishRules(p.dragRules, p.dragFlags))
+			}
+			accept, ok := p.GetIntAttribute("Accept")
+			if !ok {
+				accept = 0
+			}
+			if accept == 0 {
+				fmt.Fprint(&str, " Any card may be placed on an empty tableaux.")
+			} else if accept > 0 && accept < 14 {
+				var cardValueEnglish [14]string = [14]string{"", "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+				fmt.Fprintf(&str, " Only a %s may be placed on an empty tableaux.", cardValueEnglish[accept])
+			} else {
+				fmt.Fprint(&str, " No card may be placed on an empty tableaux.")
+			}
+			if p.dragFlags&1 == 1 {
+				fmt.Fprint(&str, " Only a single card may be moved at once.")
+			}
+			if p.buildFlags&2 == 2 {
+				fmt.Fprint(&str, " Strictly, only the top card of each stack may be moved. However, the game automates moves of several cards, when empty tableau columns and empty cells allow.")
+			}
+			// TODO Bury, Disinter
+		case "Cell":
+			fmt.Fprint(&str, "Cell: Can store one card of any type.")
+		case "Reserve":
+			fmt.Fprint(&str, "Reserve: stores multiple cards of any type. You cannot move a card to a reserve.")
 		}
 		rules = append(rules, str.String())
 	}
