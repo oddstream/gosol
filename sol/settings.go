38c89adcc1694fcc90cbd17474325609ee9343ff
oddstream <oddstream@googlemail.com>
2023-01-03 18:50:27 +0000

    change to movable card highlighting

--- sol/settings.go
+++ sol/settings.go
@@ -5,10 +5,11 @@ func ShowSettingsDrawer() {
 	// consider using callbacks so UI can query each setting
 	var booleanSettings = map[string]bool{
 		// "FixedCards":    ThePreferences.FixedCards,
-		"PowerMoves":    ThePreferences.PowerMoves,
-		"ColorfulCards": ThePreferences.ColorfulCards,
-		"MirrorBaize":   ThePreferences.MirrorBaize,
-		"Mute":          ThePreferences.Mute,
+		"PowerMoves":       ThePreferences.PowerMoves,
+		"ColorfulCards":    ThePreferences.ColorfulCards,
+		"MirrorBaize":      ThePreferences.MirrorBaize,
+		"ShowMovableCards": ThePreferences.ShowMovableCards,
+		"Mute":             ThePreferences.Mute,
 	}
 	TheUI.ShowSettingsDrawer(booleanSettings)
 }
